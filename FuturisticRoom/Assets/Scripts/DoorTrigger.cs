01d0a1f8d3e2cede8b26795e2a320873af87f035
sophie.amelin <42768125+sophie333@users.noreply.github.com>
2018-11-08 18:18:38 +0100

    Added Skybox and Wardrobe Interaction
    added custom hdr skybox, added animation, sounds to wardrobe door

---
+++ FuturisticRoom/Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+using TMPro;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private Animator anim;
+
+    [SerializeField]
+    private AudioSource s_slidingDoorOpen;
+
+
+    [SerializeField]
+    private AudioSource s_slidingDoorClose;
+
+    [SerializeField]
+    private GameObject t_openDoor;
+
+    private bool opened = false;
+
+    private void OnTriggerStay(Collider other)
+    {
+        FirstPersonController player = other.GetComponent<FirstPersonController>();
+
+        if (player && !opened)
+        {
+            t_openDoor.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+                opened = true;
+                s_slidingDoorOpen.Play();
+                anim.Play("DoorOpen");
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        FirstPersonController player = other.GetComponent<FirstPersonController>();
+
+        if (player)
+        {
+            t_openDoor.SetActive(false);
+
+            if (opened)
+            {
+                opened = false;
+                s_slidingDoorClose.Play();
+                anim.Play("DoorClose");
+            }
+        }
+    }
+}
